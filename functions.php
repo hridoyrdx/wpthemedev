5f52bbbde371187bc61b8cc7ed6773fc417f61c7
hridoyrdx <smallrdx@gmail.com>
2021-02-09 19:15:22 +0600

    theme new changes

--- functions.php
+++ functions.php
@@ -1,9 +1,14 @@
 <?php
 add_theme_support('title-tag');
+add_theme_support('post-thumbnails');
 
 function wp_top_menu(){
   register_nav_menu('header-top', __('Header Main Menu'));
 }
 add_action('init', 'wp_top_menu');
+
+
+
+
 
  ?>
--- index.php
+++ index.php
@@ -1,170 +1,5 @@
 <?php get_header(); ?>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="top-left pull-left">
-            <div class="language">
-              <form action="#" method="post" enctype="multipart/form-data" id="language">
-                <div class="btn-group">
-                  <button class="btn btn-link dropdown-toggle" data-toggle="dropdown" aria-expanded="false"> English <span class="caret"></span></button>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Arabic</a></li>
-                    <li><a href="#"> English</a></li>
-                  </ul>
-                </div>
-              </form>
-            </div>
-            <div class="currency">
-              <form action="#" method="post" enctype="multipart/form-data" id="currency">
-                <div class="btn-group">
-                  <button class="btn btn-link dropdown-toggle" data-toggle="dropdown"> <strong>USD</strong> <span class="caret"></span> </button>
-                  <ul class="dropdown-menu">
-                    <li><a href="#">Euro</a></li>
-                    <li><a href="#">Pound</a></li>
-                    <li><a href="#">USD</a></li>
-                  </ul>
-                </div>
-              </form>
-            </div>
-            <div class="wel-come-msg"> Welcome to our online store! </div>
-          </div>
-          <div class="top-right pull-right">
-            <div id="top-links" class="nav pull-right">
-              <ul class="list-inline">
-                <li class="dropdown"><a href="#" title="My Account" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user" aria-hidden="true"></i><span>My Account</span> <span class="caret"></span></a>
-                  <ul class="dropdown-menu dropdown-menu-right">
-                    <li><a href="register.html">Register</a></li>
-                    <li><a href="login.html">Login</a></li>
-                  </ul>
-                </li>
-                <li><a href="#" id="wishlist-total" title="Wish List (0)"><i class="fa fa-heart" aria-hidden="true"></i><span>Wish List</span><span> (0)</span></a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="container">
-    <div class="header-inner">
-      <div class="col-sm-3 col-xs-3 header-left">
-        <div id="logo"> <a href="index-2.html"><img src="<?php echo get_template_directory_uri(); ?>/image/logo.png" title="E-Commerce" alt="E-Commerce" class="img-responsive" /></a> </div>
-      </div>
-      <div class="col-sm-9 col-xs-9 header-right">
-        <div id="search" class="input-group">
-          <input type="text" name="search" value="" placeholder="Enter your keyword ..." class="form-control input-lg" />
-          <span class="input-group-btn">
-          <button type="button" class="btn btn-default btn-lg"><span>Search</span></button>
-          </span> </div>
-        <div id="cart" class="btn-group btn-block">
-          <button type="button" class="btn btn-inverse btn-block btn-lg dropdown-toggle cart-dropdown-button"> <span id="cart-total"><span>Shopping Cart</span><br>
-          3 item(s) - $254.00</span></button>
-          <ul class="dropdown-menu pull-right cart-dropdown-menu">
-            <li>
-              <table class="table table-striped">
-                <tbody>
-                  <tr>
-                    <td class="text-center"><a href="#"><img class="img-thumbnail" title="lorem ippsum dolor dummy" alt="lorem ippsum dolor dummy" src="<?php echo get_template_directory_uri(); ?>/image/product/7product56x72.jpg"></a></td>
-                    <td class="text-left"><a href="#">lorem ippsum dolor dummy</a></td>
-                    <td class="text-right">x 1</td>
-                    <td class="text-right">$254.00</td>
-                    <td class="text-center"><button class="btn btn-danger btn-xs" title="Remove" type="button"><i class="fa fa-times"></i></button></td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
-            <li>
-              <table class="table table-striped">
-                <tbody>
-                  <tr>
-                    <td class="text-center"><a href="#"><img class="img-thumbnail" title="lorem ippsum dolor dummy" alt="lorem ippsum dolor dummy" src="<?php echo get_template_directory_uri(); ?>/image/product/7product56x72.jpg"></a></td>
-                    <td class="text-left"><a href="#">lorem ippsum dolor dummy</a></td>
-                    <td class="text-right">x 1</td>
-                    <td class="text-right">$254.00</td>
-                    <td class="text-center"><button class="btn btn-danger btn-xs" title="Remove" type="button"><i class="fa fa-times"></i></button></td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
-            <li>
-              <table class="table table-striped">
-                <tbody>
-                  <tr>
-                    <td class="text-center"><a href="#"><img class="img-thumbnail" title="lorem ippsum dolor dummy" alt="lorem ippsum dolor dummy" src="<?php echo get_template_directory_uri(); ?>/image/product/7product56x72.jpg"></a></td>
-                    <td class="text-left"><a href="#">lorem ippsum dolor dummy</a></td>
-                    <td class="text-right">x 1</td>
-                    <td class="text-right">$254.00</td>
-                    <td class="text-center"><button class="btn btn-danger btn-xs" title="Remove" type="button"><i class="fa fa-times"></i></button></td>
-                  </tr>
-                </tbody>
-              </table>
-            </li>
-            <li>
-              <div>
-                <table class="table table-bordered">
-                  <tbody>
-                    <tr>
-                      <td class="text-right"><strong>Sub-Total</strong></td>
-                      <td class="text-right">$210.00</td>
-                    </tr>
-                    <tr>
-                      <td class="text-right"><strong>Eco Tax (-2.00)</strong></td>
-                      <td class="text-right">$2.00</td>
-                    </tr>
-                    <tr>
-                      <td class="text-right"><strong>VAT (20%)</strong></td>
-                      <td class="text-right">$42.00</td>
-                    </tr>
-                    <tr>
-                      <td class="text-right"><strong>Total</strong></td>
-                      <td class="text-right">$254.00</td>
-                    </tr>
-                  </tbody>
-                </table>
-                <p class="text-right"> <span class="btn-viewcart"><a href="cart.html"><strong><i class="fa fa-shopping-cart"></i> View Cart</strong></a></span> <span class="btn-checkout"><a href="checkout.html"><strong><i class="fa fa-share"></i> Checkout</strong></a></span> </p>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>
-<nav id="menu" class="navbar">
-  <div class="nav-inner">
-    <div class="navbar-header"><span id="category" class="visible-xs">Categories</span>
-      <button type="button" class="btn btn-navbar navbar-toggle" ><i class="fa fa-bars"></i></button>
-    </div>
-    <div class="navbar-collapse">
-      <ul class="main-navigation">
-        <?php wp_nav_menu(array(
-          'theme_location'  => "header-top"
-        )); ?>
-        <!-- <li><a href="index-2.html"   class="parent"  >Home</a> </li>
-        <li><a href="category.html"   class="parent"  >Collection</a> </li>
-        <li><a href="category.html"   class="parent"  >Women</a> </li>
-        <li><a href="category.html"   class="parent"  >Men</a> </li>
-        <li><a href="#" class="active parent">Page</a>
-          <ul>
-            <li><a href="category.html">Category Page</a></li>
-            <li><a href="cart.html">Cart Page</a></li>
-            <li><a href="checkout.html">Checkout Page</a></li>
-            <li><a href="blog.html" >Blog Page</a></li>
-            <li><a href="single-blog.html" >Singale Blog Page</a></li>
-            <li><a href="register.html">Register Page</a></li>
-            <li><a href="contact.html">Contact Page</a></li>
-            <li><a href="affiliate.html">Affiliate</a></li>
-            <li><a href="forgetpassword.html">Forget Password</a></li>
-          </ul>
-        </li>
-        <li><a href="blog.html" class="parent"  >Blog</a></li>
-        <li><a href="about-us.html" >About us</a></li>
-        <li><a href="contact.html" >Contact Us</a> </li> -->
-      </ul>
-    </div>
-  </div>
-</nav>
 <div class="mainbanner">
   <div id="main-banner" class="owl-carousel home-slider">
     <div class="item"> <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/banners/Main-Banner1.jpg" alt="main-banner1" class="img-responsive" /></a> </div>
@@ -1325,46 +1160,22 @@
             </div>
             <div class="blog-inner box">
               <ul class="list-unstyled blog-wrapper" id="latest-blog">
+                <?php if(have_posts()) : while(have_posts()): the_post(); ?>
+                
                 <li class="item blog-slider-item">
                   <div class="blog1 blog">
-                    <div class="blog-<?php echo get_template_directory_uri(); ?>/image"> <a href="#" class="blog-<?php echo get_template_directory_uri(); ?>/imagelink"><img src="<?php echo get_template_directory_uri(); ?>/image/blog/blog_1.jpg" alt="#"></a> <span class="blog-hover"></span> <span class="blog-readmore-outer"></span> </div>
+                    <div class="blog-image"> <a href="#" class="blog-imagelink"><?php the_post_thumbnail(); ?></a> <span class="blog-hover"></span> <span class="blog-readmore-outer"></span> </div>
                     <div class="blog-content">
-                      <h2 class="blog-name"><a href="#" >Putamus parum claram, anteposuerit the new of model litterarum formas ...</a></h2>
-                      <span class="blog-author">Poted by: <a href="#">Lionode Themes</a></span><span class="blog-date">Jun 29, 2017</span> <a href="single-blog.html" class="blog-readmore">Read more</a> </div>
+                      <h2 class="blog-name"><a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></h2>
+                      <span class="blog-author">Poted by: <a href="#"><?php the_author(); ?></a></span><span class="blog-date"><?php the_date(); ?></span> <a href="<?php the_permalink(); ?>" class="blog-readmore">Read more</a> </div>
                   </div>
                 </li>
-                <li class="item blog-slider-item">
-                  <div class="blog2 blog">
-                    <div class="blog-<?php echo get_template_directory_uri(); ?>/image"> <a href="#" class="blog-<?php echo get_template_directory_uri(); ?>/imagelink"><img src="<?php echo get_template_directory_uri(); ?>/image/blog/blog_2.jpg" alt="#"></a> <span class="blog-hover"></span> <span class="blog-readmore-outer"></span> </div>
-                    <div class="blog-content">
-                      <h2 class="blog-name"><a href="#" >Putamus parum claram, anteposuerit the new of model litterarum formas ...</a></h2>
-                      <span class="blog-author">Poted by: <a href="#">Lionode Themes</a></span><span class="blog-date">Jun 29, 2017</span> <a href="single-blog.html" class="blog-readmore">Read more</a> </div>
-                  </div>
-                </li>
-                <li class="item blog-slider-item">
-                  <div class="blog3 blog">
-                    <div class="blog-<?php echo get_template_directory_uri(); ?>/image"> <a href="#" class="blog-<?php echo get_template_directory_uri(); ?>/imagelink"><img src="<?php echo get_template_directory_uri(); ?>/image/blog/blog_3.jpg" alt="#"></a> <span class="blog-hover"></span> <span class="blog-readmore-outer"></span> </div>
-                    <div class="blog-content">
-                      <h2 class="blog-name"><a href="#" >Putamus parum claram, anteposuerit the new of model litterarum formas ...</a></h2>
-                      <span class="blog-author">Poted by: <a href="#">Lionode Themes</a> </span><span class="blog-date">Jun 29, 2017</span> <a href="single-blog.html" class="blog-readmore">Read more</a> </div>
-                  </div>
-                </li>
-                <li class="item blog-slider-item">
-                  <div class="blog4 blog">
-                    <div class="blog-<?php echo get_template_directory_uri(); ?>/image"> <a href="#" class="blog-<?php echo get_template_directory_uri(); ?>/imagelink"><img src="<?php echo get_template_directory_uri(); ?>/image/blog/blog_4.jpg" alt="#"></a> <span class="blog-hover"></span> <span class="blog-readmore-outer"></span> </div>
-                    <div class="blog-content">
-                      <h2 class="blog-name"><a href="#" >Putamus parum claram, anteposuerit the new of model litterarum formas ...</a></h2>
-                      <span class="blog-author">Poted by: <a href="#">Lionode Themes</a></span><span class="blog-date">Jun 29, 2017</span> <a href="single-blog.html" class="blog-readmore">Read more</a> </div>
-                  </div>
-                </li>
-                <li class="item blog-slider-item">
-                  <div class="blog5 blog">
-                    <div class="blog-<?php echo get_template_directory_uri(); ?>/image"> <a href="#" class="blog-<?php echo get_template_directory_uri(); ?>/imagelink"><img src="<?php echo get_template_directory_uri(); ?>/image/blog/blog_5.jpg" alt="#"></a> <span class="blog-hover"></span> <span class="blog-readmore-outer"></span> </div>
-                    <div class="blog-content">
-                      <h2 class="blog-name"><a href="#" >Putamus parum claram, anteposuerit the new of model litterarum formas ...</a></h2>
-                      <span class="blog-author">Poted by: <a href="#">Lionode Themes</a></span><span class="blog-date">Jun 29, 2017</span> <a href="single-blog.html" class="blog-readmore">Read more</a> </div>
-                  </div>
-                </li>
+
+                <?php 
+                endwhile;
+                endif; ?>
+                
+              
               </ul>
             </div>
           </div>
